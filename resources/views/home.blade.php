8e2a838b78a333eff82a84e21018610c8ec54ea5
KauaLima678 <kaua26kklima@gmail.com>
2026-07-25 13:48:24 -0300

    feat: setup tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>{{ config('app.name') }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <h1>Welcome to the HomePage</h1>
-    <h2>Olá {{ $name }}</h2>
+    <h1 class="font-bold text-3xl text-green-900">Welcome to the HomePage</h1>
+    <h2 class="text-red-900 text-4xl font-bold">Olá {{ $name }}</h2>
     <p>Seus hábitos são</p>
     <ul>
         @foreach ( $habits as $habit )
