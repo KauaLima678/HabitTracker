bd7e350c4eefdd815112f1e0290f2b4e0f9801e3
KauaLima678 <kaua26kklima@gmail.com>
2026-07-28 01:32:53 -0300

    feat(Auth): add login functionality and user session management

--- app/Models/User.php
+++ app/Models/User.php
@@ -32,7 +32,7 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'passoword' => 'hashed'
+            'password' => 'hashed'
         ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [SiteController::class, 'index']);
+
+Route::get('/login', [AuthController::class, 'index'] );
 
 // MVC
 // Model -> Interação do banco de dados
